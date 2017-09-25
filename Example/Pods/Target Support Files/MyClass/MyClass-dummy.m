0b2fd6e441eccece1c0ddcdd724e004aaf0d3896
王更 <wangg@yunyichina.cn>
2017-09-25 17:56:10 +0800

    0.1.0

---
+++ Example/Pods/Target Support Files/MyClass/MyClass-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MyClass : NSObject
+@end
+@implementation PodsDummy_MyClass
+@end
---
+++ Example/Pods/Target Support Files/Pods-MyClass_Tests/Pods-MyClass_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyClass_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_MyClass_Tests
+@end
